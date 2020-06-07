2f5dbb480598e570eb4d1b88693824f017ee6e22
Sabrina <sabrinamonteiro3@outlook.com>
2020-06-07 16:08:38 -0300

    #FT-01#
    - criei as paginas de login e senha;
    - add os componentes, mas precisa corrigir algumas coisas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrerComponent } from './registrer/registrer.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		LoginComponent,
+		RegistrerComponent,
 	],
 	imports: [
 		BrowserModule,
