83e5353be819b9eb0208c0547359a8cea6ed25d2
Sabrina <sabrinamonteiro3@outlook.com>
2020-07-04 19:58:19 -0300

    #FT-06# - Inclusion of dashboard data

--- src/app/internal/dashboard/dashboard.component.ts
+++ src/app/internal/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DashService } from 'src/app/services/dash.service';
+import { NotificationService } from 'src/app/services/notification.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+	usuario: User;
+	nextExchange: number;
+	parts: number;
+	mainReason: string;
+	category: string;
 
-  ngOnInit(): void {
+  constructor(private userService: UserService, private dashService: DashService, private notifyService: NotificationService) {
+
   }
 
+  ngOnInit(): void {
+
+	//#FT-06# - Inclusion of dashboard data
+	this.userService.read()
+	.subscribe(ret => {
+		this.usuario = ret.user;
+	}, error => {
+		this.notifyService.showError("Não foi possível carregar seus dados", "Erro!");
+	});
+
+	this.dashService.getMainReason()
+	.subscribe(ret => {
+		this.mainReason = ret.item;
+	}, error => {
+		this.notifyService.showError("Não foi possível carregar seus dados", "Erro!");
+	});
+
+	this.dashService.getCategory()
+	.subscribe(ret => {
+		this.category = ret.item;
+	}, error => {
+		this.notifyService.showError("Não foi possível carregar seus dados", "Erro!");
+	});
+
+	this.dashService.getParts()
+	.subscribe(ret => {
+		this.parts = ret.item;
+	}, error => {
+		this.notifyService.showError("Não foi possível carregar seus dados", "Erro!");
+	});
+
+	this.dashService.getNextExchange()
+	.subscribe(ret => {
+		this.nextExchange = ret.item;
+	}, error => {
+		this.notifyService.showError("Não foi possível carregar seus dados", "Erro!");
+	});
+
+  }
 }
+
+
